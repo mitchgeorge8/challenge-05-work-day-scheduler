1ece7982b3ebfbda2918c772e7752c4d68e2fceb
Mitchell George <mitchgeorge8@gmail.com>
2022-03-13 01:34:01 -0500

    Save event blocks to local storage with save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,75 @@
 let DateTime = luxon.DateTime;
-
 const now = DateTime.now();
 
-$("#currentDay").text(now.toLocaleString(DateTime.DATE_HUGE));
+let events = [];
+
+// set currentDay to the current date using Luxon
+$('#currentDay').text(now.toLocaleString(DateTime.DATE_HUGE));
 
 let auditSchedule = function() {
+    // loop through all of the time blocks
     for (i = 8; i < 21; i++) {
+        // get current hour and block id
         let hourNow = now.toFormat('H');
-        let block = $("#block-" + i);
+        let block = $('#block-' + i);
 
         if (i < hourNow) {
-            block.addClass("past");
-            block.removeClass("present");
-            block.removeClass("future");
+            block.addClass('past');
+            block.removeClass('present');
+            block.removeClass('future');
         }
         else if (i == hourNow) {
-            block.removeClass("past");
-            block.addClass("present");
-            block.removeClass("future");
+            block.removeClass('past');
+            block.addClass('present');
+            block.removeClass('future');
         }
         else if (i > hourNow) {
-            block.removeClass("past");
-            block.removeClass("present");
-            block.addClass("future");
+            block.removeClass('past');
+            block.removeClass('present');
+            block.addClass('future');
         }
     };
 };
+
+let saveEvents = function() {
+    localStorage.setItem('events', JSON.stringify(events));
+};
+
+$('.time-block').on('click', '.saveBtn', function() {
+    // find the corresponding textarea
+    let timeBlockEl =  $(this).closest('.time-block');
+    let eventEl = timeBlockEl.find('textarea');
+    
+    // get event text
+    let text = eventEl.val().trim();
+    if (text === "") {
+        return false;
+    }
+
+    // get the text area id
+    let eventId = eventEl.attr('id');
+    
+    // create event object
+    let event = {
+        text: text,
+        id: eventId
+    };
+
+    // if the events array contains nothing, simply push new event
+    if (events.length === 0) {
+        events.push(event);
+    }
+    else {
+        // loop through events and delete any existing event with matching id
+        for (i = 0; i < events.length; i++) {
+            if (events[i].id === event.id) {
+                events.splice(i, 1);
+            }
+        };
+        events.push(event);
+    }
+
+    saveEvents();
+});
 
 auditSchedule();
