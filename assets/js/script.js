12de94c997464ccf2c5bb37dae635fe921557c1e
Mitchell George <mitchgeorge8@gmail.com>
2022-03-12 19:13:34 -0500

    Color code the time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ const now = DateTime.now();
 
 $("#currentDay").text(now.toLocaleString(DateTime.DATE_HUGE));
 
+let auditSchedule = function() {
+    for (i = 8; i < 21; i++) {
+        let hourNow = now.toFormat('H');
+        let block = $("#block-" + i);
+
+        if (i < hourNow) {
+            block.addClass("past");
+            block.removeClass("present");
+            block.removeClass("future");
+        }
+        else if (i == hourNow) {
+            block.removeClass("past");
+            block.addClass("present");
+            block.removeClass("future");
+        }
+        else if (i > hourNow) {
+            block.removeClass("past");
+            block.removeClass("present");
+            block.addClass("future");
+        }
+    };
+};
+
+auditSchedule();
